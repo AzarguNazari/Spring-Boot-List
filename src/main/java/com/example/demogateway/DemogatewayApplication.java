c134bcaeea462aa6fe87dfb6f18fb7b7e9c5c6e1
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-07-23 23:53:35 +0200

    added cloud gateway

---
+++ src/main/java/com/example/demogateway/DemogatewayApplication.java
@@ -0,0 +1,22 @@
+package com.example.demogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemogatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemogatewayApplication.class, args);
+	}
+}
